bc871ccade60560b6018543ecc625d4be71e7023
Mildred Dube <snagfitms@gmail.com>
2026-06-11 00:34:18 +0200

    Set up Express server for booking API

--- booking-system/server.js
+++ booking-system/server.js
@@ -1,1 +1,13 @@
+const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Booking API is running...");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
